fc09d444c7f5e165351f9ec399a8ded4dcc220d6
Mik-design <marble10@mail.ru>
2021-07-15 09:43:51 +0600

    1)added dependencies to gradle 2)created repo package 3) created WatherRepo &MainViewModel classes

---
+++ app/src/main/java/kg/tutorial/weather_app56/repo/WeatherRepo.kt
@@ -0,0 +1,13 @@
+package kg.tutorial.weather_app56.repo
+
+import kg.tutorial.weather_app56.network.WeatherApi
+import kg.tutorial.weather_app56.storage.ForeCastDatabase
+
+class WeatherRepo {
+    private val db: ForeCastDatabase,
+    private val weatherApi: WeatherApi
+}
+
+{
+
+}
---
+++ app/src/main/java/kg/tutorial/weather_app56/ui/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kg.tutorial.weather_app56.ui
+
+import androidx.lifecycle.ViewModel
+import kg.tutorial.weather_app56.repo.WeatherRepo
+
+class MainViewModel(private val weatherRepo: WeatherRepo): ViewModel (){
+}
